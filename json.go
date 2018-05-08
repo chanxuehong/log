f187db77492066f3265df7e7e26f0a9a9ddcbfb3
chanxuehong <chanxuehong@gmail.com>
2018-05-08 12:17:47 +0800

    log.JSON

--- json.go
+++ json.go
@@ -1,12 +1,20 @@
 package log
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+)
 
 // JSON is a helper function, following is it's code.
 //
 //  data, _ := json.Marshal(v)
 //  return string(data)
 func JSON(v interface{}) string {
-	data, _ := json.Marshal(v)
-	return string(data)
+	buffer := _bufferPool.Get().(*bytes.Buffer)
+	defer _bufferPool.Put(buffer)
+	buffer.Reset()
+	if err := json.NewEncoder(buffer).Encode(v); err != nil {
+		return ""
+	}
+	return string(buffer.Bytes())
 }
