e5ad3b0356bc1b3a36f1d2bd7cca1369c75d330c
chanxuehong <chanxuehong@gmail.com>
2018-05-14 09:08:11 +0800

    text format modify

--- text_formatter.go
+++ text_formatter.go
@@ -18,26 +18,42 @@ func (f *textFormatter) Format(entry *Entry) ([]byte, error) {
 	} else {
 		buffer = bytes.NewBuffer(make([]byte, 0, 16<<10))
 	}
-	if len(entry.Fields) > 0 {
-		prefixFieldClashes(entry.Fields)
-	}
 	f.appendKeyValue(buffer, fieldKeyTime, FormatTimeString(entry.Time.In(_beijingLocation)))
 	f.appendKeyValue(buffer, fieldKeyLevel, entry.Level.String())
 	f.appendKeyValue(buffer, fieldKeyTraceId, entry.TraceId)
 	f.appendKeyValue(buffer, fieldKeyLocation, entry.Location)
 	f.appendKeyValue(buffer, fieldKeyMessage, entry.Message)
-
-	keys := make([]string, 0, len(entry.Fields))
-	for k := range entry.Fields {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		v := entry.Fields[k]
-		f.appendKeyValue(buffer, k, v)
+	if len(entry.Fields) > 0 {
+		prefixFieldClashes(entry.Fields)
+		keys := make([]string, 0, len(entry.Fields))
+		for k := range entry.Fields {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			v := entry.Fields[k]
+			f.appendKeyValue(buffer, k, v)
+		}
 	}
 	buffer.WriteByte('\n')
 	return buffer.Bytes(), nil
+}
+
+func (f *textFormatter) appendKeyValue(b *bytes.Buffer, key string, value interface{}) {
+	if b.Len() > 0 {
+		b.WriteString(", ")
+	}
+	b.WriteString(key)
+	b.WriteByte('=')
+	f.appendValue(b, value)
+}
+
+func (f *textFormatter) appendValue(b *bytes.Buffer, value interface{}) {
+	stringVal, ok := value.(string)
+	if !ok {
+		stringVal = fmt.Sprint(value)
+	}
+	b.WriteString(stringVal)
 }
 
 var _beijingLocation = time.FixedZone("Asia/Shanghai", 8*60*60)
@@ -71,21 +87,4 @@ func prefixFieldClashes(data map[string]interface{}) {
 		data["fields."+fieldKeyMessage] = v
 		delete(data, fieldKeyMessage)
 	}
-}
-
-func (f *textFormatter) appendKeyValue(b *bytes.Buffer, key string, value interface{}) {
-	if b.Len() > 0 {
-		b.WriteString(", ")
-	}
-	b.WriteString(key)
-	b.WriteByte('=')
-	f.appendValue(b, value)
-}
-
-func (f *textFormatter) appendValue(b *bytes.Buffer, value interface{}) {
-	stringVal, ok := value.(string)
-	if !ok {
-		stringVal = fmt.Sprint(value)
-	}
-	b.WriteString(stringVal)
 }
