d1a936848830ded170648d6f96d5bf6b4bfc142c
chanxuehong <chanxuehong@gmail.com>
2019-10-31 17:14:22 +0800

    Revert "rm some tests"
    This reverts commit 8af0b682e5ef54e008e1c5461993fbe89088dec8.

--- buffer_pool_test.go
+++ buffer_pool_test.go
@@ -5,6 +5,142 @@ import (
 	"testing"
 )
 
+func TestDefaultBytesBufferPool(t *testing.T) {
+	// new
+	func() {
+		pool := getBytesBufferPool()
+
+		buffer := pool.Get()
+		defer pool.Put(buffer)
+
+		have := buffer.String()
+		want := ""
+		if have != want {
+			t.Errorf("have:%s, want:%s", have, want)
+			return
+		}
+	}()
+
+	// clean up
+	{
+		pool := getBytesBufferPool()
+		for i := 0; i < 10; i++ {
+			pool.Get()
+		}
+	}
+
+	// new twice
+	func() {
+		pool := getBytesBufferPool()
+
+		buffer := pool.Get()
+		defer pool.Put(buffer)
+
+		have := buffer.String()
+		want := ""
+		if have != want {
+			t.Errorf("have:%s, want:%s", have, want)
+			return
+		}
+		buffer.WriteString("buffer")
+
+		buffer2 := pool.Get()
+		defer pool.Put(buffer2)
+
+		have = buffer2.String()
+		want = ""
+		if have != want {
+			t.Errorf("have:%s, want:%s", have, want)
+			return
+		}
+	}()
+
+	// clean up
+	{
+		pool := getBytesBufferPool()
+		for i := 0; i < 10; i++ {
+			pool.Get()
+		}
+	}
+
+	// reuse
+	func() {
+		func() {
+			pool := getBytesBufferPool()
+
+			buffer := pool.Get()
+			defer pool.Put(buffer)
+
+			have := buffer.String()
+			want := ""
+			if have != want {
+				t.Errorf("have:%s, want:%s", have, want)
+				return
+			}
+			buffer.WriteString("buffer")
+		}()
+
+		func() {
+			pool := getBytesBufferPool()
+
+			buffer := pool.Get()
+			defer pool.Put(buffer)
+
+			have := buffer.String()
+			want := "buffer"
+			if have != want {
+				t.Errorf("have:%s, want:%s", have, want)
+				return
+			}
+		}()
+	}()
+
+	// clean up
+	{
+		pool := getBytesBufferPool()
+		for i := 0; i < 10; i++ {
+			pool.Get()
+		}
+	}
+
+	// reuse and put nil
+	func() {
+		func() {
+			pool := getBytesBufferPool()
+
+			buffer := pool.Get()
+			defer pool.Put(buffer)
+
+			have := buffer.String()
+			want := ""
+			if have != want {
+				t.Errorf("have:%s, want:%s", have, want)
+				return
+			}
+			buffer.WriteString("buffer")
+		}()
+
+		pool := getBytesBufferPool()
+		for i := 0; i < 10; i++ {
+			pool.Put(nil)
+		}
+
+		func() {
+			pool := getBytesBufferPool()
+
+			buffer := pool.Get()
+			defer pool.Put(buffer)
+
+			have := buffer.String()
+			want := "buffer"
+			if have != want {
+				t.Errorf("have:%s, want:%s", have, want)
+				return
+			}
+		}()
+	}()
+}
+
 func Test_SetBytesBufferPool_GetBytesBufferPool(t *testing.T) {
 	defer SetBytesBufferPool(_defaultBytesBufferPool)
 
