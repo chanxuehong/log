32033f2bb6edbaa4ec36ee939492b2bf05cccf54
wwq8875 <2992395632@qq.com>
2019-10-31 11:38:28 +0800

    simplify code

--- noop_logger.go
+++ noop_logger.go
@@ -6,53 +6,53 @@ import "io"
 type NoopLogger struct{}
 
 // Fatal impl Logger Fatal
-func (l *NoopLogger) Fatal(msg string, fields ...interface{}) {
+func (NoopLogger) Fatal(msg string, fields ...interface{}) {
 }
 
 // Error impl Logger Error
-func (l *NoopLogger) Error(msg string, fields ...interface{}) {
+func (NoopLogger) Error(msg string, fields ...interface{}) {
 }
 
 // Warn impl Logger Warn
-func (l *NoopLogger) Warn(msg string, fields ...interface{}) {
+func (NoopLogger) Warn(msg string, fields ...interface{}) {
 }
 
 // Info impl Logger Info
-func (l *NoopLogger) Info(msg string, fields ...interface{}) {
+func (NoopLogger) Info(msg string, fields ...interface{}) {
 }
 
 // Debug impl Logger Debug
-func (l *NoopLogger) Debug(msg string, fields ...interface{}) {
+func (NoopLogger) Debug(msg string, fields ...interface{}) {
 }
 
 // Output impl Logger Output
-func (l *NoopLogger) Output(calldepth int, level Level, msg string, fields ...interface{}) {
+func (NoopLogger) Output(calldepth int, level Level, msg string, fields ...interface{}) {
 }
 
 // WithField impl Logger WithField
-func (l *NoopLogger) WithField(key string, value interface{}) Logger {
-	return l
+func (NoopLogger) WithField(key string, value interface{}) Logger {
+	return NoopLogger{}
 }
 
 // WithFields impl Logger WithFields
-func (l *NoopLogger) WithFields(fields ...interface{}) Logger {
-	return l
+func (NoopLogger) WithFields(fields ...interface{}) Logger {
+	return NoopLogger{}
 }
 
 // SetFormatter impl Logger SetFormatter
-func (l *NoopLogger) SetFormatter(Formatter) {
+func (NoopLogger) SetFormatter(Formatter) {
 }
 
 // SetOutput impl Logger SetOutput
-func (l *NoopLogger) SetOutput(io.Writer) {
+func (NoopLogger) SetOutput(io.Writer) {
 }
 
 // SetLevel impl Logger SetLevel
-func (l *NoopLogger) SetLevel(Level) error {
+func (NoopLogger) SetLevel(Level) error {
 	return nil
 }
 
 // SetLevelString impl Logger SetLevelString
-func (l *NoopLogger) SetLevelString(string) error {
+func (NoopLogger) SetLevelString(string) error {
 	return nil
 }
